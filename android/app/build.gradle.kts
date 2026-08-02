82fcd70cad90fdb498ed41a73ff8d6615d1380f6
ceciliowildnei <wildnei@prof.educacao.sp.gov.br>
2026-08-01 21:32:17 -0300

    Adiciona android/app/build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.queijoswr.pedidos"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.queijoswr.pedidos"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
