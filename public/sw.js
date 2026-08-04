98067a828688a1604cf71cfad32f0883cc62290c
ceciliowildnei <wildnei@prof.educacao.sp.gov.br>
2026-08-03 22:23:59 -0300

    pwa: adicionar cache e suporte offline

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,35 @@
-self.addEventListener('install', () => self.skipWaiting());
+const CACHE_NAME = 'queijos-wr-v2';
+const APP_SHELL = ['/', '/manifest.webmanifest', '/brand/app-icon.svg', '/brand/app-icon-192.webp', '/install-pwa.css', '/install-pwa.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => undefined));
+  self.skipWaiting();
+});
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put('/', copy));
+      return response;
+    }).catch(() => caches.match('/')));
+    return;
+  }
+
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response.ok) {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  })));
 });
